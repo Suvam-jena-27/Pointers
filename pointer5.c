187c0608b71128f7323ccd56efd4e27b08ade033
Your name <your.email@example.com>
2026-09-07 18:10:48 +0530

    Arrays as fxn arguments

--- pointer5.c
+++ pointer5.c
@@ -2,6 +2,21 @@
 
 /*4. Arrays as fxn. arg.*/
 
+int SOE(int A[], int n){
+    int i;
+    int sum = 0;
+    
+    for(i = 0; i < n; i++){
+        sum += A[i];
+    }
+    return sum;
+}
+
 int main(){
+    int A[] = {1, 2, 3, 4, 5};
+    int size = sizeof(A)/sizeof(A[0]);
+    int total = SOE(A, size);
+
+    printf("Sum of element: %d\n", total);
     return 0;
 }
