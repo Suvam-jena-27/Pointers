87d32b848b862ef1bd5816ab537e960981026d7f
Your name <your.email@example.com>
2026-09-07 18:27:37 +0530

    Arrays as fxn, arg.

--- pointer5.c
+++ pointer5.c
@@ -2,20 +2,20 @@
 
 /*4. Arrays as fxn. arg.*/
 
-int SOE(int A[], int n){
-    int i;
+int SOE(int A[], int n){    //A array in passed by reference here
+    int i;                  //Hence, the sizeof(A) in SOE() is 4 bytes
     int sum = 0;
     
     for(i = 0; i < n; i++){
-        sum += A[i];
+        sum += A[i];        //interpreted as *(A+i)
     }
     return sum;
 }
 
 int main(){
     int A[] = {1, 2, 3, 4, 5};
-    int size = sizeof(A)/sizeof(A[0]);
-    int total = SOE(A, size);
+    int size = sizeof(A)/sizeof(A[0]);  //the sizeof(A) in main() is 20 bytes
+    int total = SOE(A, size);           //SOE(&A[0], size);
 
     printf("Sum of element: %d\n", total);
     return 0;
